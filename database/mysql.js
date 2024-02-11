a7a57504c5d9ad95bfae15c9f2c6c378f069f023
sarangu <sankar.sarangu@gmail.com>
2024-02-11 14:46:44 +0530

    initial commit

--- database/mysql.js
+++ database/mysql.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var conn = mysql.createConnection({
-  host: "sql308.infinityfree.com",
-  user: "if0_35806426",
-  password: "5oHRVAqA8xOp",
-  database: "if0_35806426_ptracker",
+  host: "srv1327.hstgr.io",
+  user: "u872798809_pt_admin",
+  password: "KrazyCoders@8",
+  database: "u872798809_p_tracker",
 });
 conn.connect((err) => {
   if (err) throw err;
